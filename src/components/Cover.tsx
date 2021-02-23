0d4c23cf7a189450556daa5a15fdcf752c4ef3df
Luis Carlos Garavito Romero <lc.garavito@uniandes.edu.co>
2021-02-23 00:00:06 -0500

    .

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -7,7 +7,7 @@ type CoverProps = {
 export default function Cover(props: CoverProps) {
   return (
     <div>
-      <h1>Luis Carlos Garavito Romero</h1>
+      <h1>Luis Carlos Garavito Romero.</h1>
       <h2>🖥  Software Developer 🖥</h2>
     </div>
   )
