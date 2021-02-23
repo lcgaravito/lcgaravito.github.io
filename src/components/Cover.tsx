777ff6e7a2d9f3673b7a97c60995ba9be7e020e3
lcgaravito <luis@finco.co>
2021-02-22 23:51:58 -0500

    Test github user commits

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -8,7 +8,7 @@ export default function Cover(props: CoverProps) {
   return (
     <div>
       <h1>Luis Carlos Garavito Romero</h1>
-      <h2>🖥 Software Developer</h2>
+      <h2>🖥  Software Developer 🖥</h2>
     </div>
   )
 }
